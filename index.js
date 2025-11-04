11b930b7b1608e43b5a8c0cbb6d9c31dd0524387
LuisP Cardenas <a01254955@tec.mx>
2025-11-04 15:22:01 -0700

    Hola

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+const port = 3000; 
+
+app.listen(port), () =>{
+    console.log('Server on port', port)
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
+console.log('Server on port', port)
