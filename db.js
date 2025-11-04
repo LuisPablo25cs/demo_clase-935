5be44d301ea5451ddcbef2836d77934d5e8d3a25
LuisP Cardenas <a01254955@tec.mx>
2025-11-04 15:53:15 -0700

    ADD posts

---
+++ db.js
@@ -0,0 +1,27 @@
+import { text } from 'express';
+import pkg from 'pg'; 
+const { Pool } = pkg
+
+const pool = new Pool({
+    connectionString: process.env.DB_URL
+});
+
+export const query = (text, params) => {
+    return pool.query(text, params);
+}
+
+const initializeDatabase = async () => {
+    try {
+        await query(`
+        CREATE TABLE IF NOT EXIST posts (
+            id SERIAL PRIMARY KEY, 
+            content TEXT NOT NULL, 
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            updated_at TIMESTAMPT DEFAULT CURRENT_TIMESTAMP
+            );
+            `);
+        console.log("Database created :D")
+    } catch (err) {
+        console.log("An error has happen while creating the db", err)
+    }
+}
---
+++ src/routes/postRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import {
+    createPost, 
+    getAllPost, 
+} from '../controllers/postController.js'
+
+const router = express.Router();
+
+router.post('/', createPost); 
+router.get('/'. getAllPost);
+
+export default router; 
